122f0b8bde768066f7a38e00de1d919a2369d57f
Adnan Azim <saazim1123@gmail.com>
2019-03-09 17:17:35 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,6 +11,13 @@ def loop_iterator(number_of_times)
   
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  loop do
+    if phrase == number_of_times
+      puts phrase
+      phrase += 1
+      break
+    end
+  
   
 end
 
