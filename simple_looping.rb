7e8560276361d185b0a41b9364ff253f5f582734
Adnan Azim <saazim1123@gmail.com>
2019-03-09 17:37:54 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,44 +8,44 @@ def loop_iterator(number_of_times)
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
-  
-  
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times loop do
-    puts phrase
-  end
-  
+  number_of_times.times do 
+    puts phrase 
+  end 
 end
-
-puts loop_iterator(4)
-
-
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  number_of_times.times do 
+    puts phrase 
+  end 
   
 end
 
 def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  phrase_count = 0 
+  while phrase_count < number_of_times 
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase 
+    phrase_count += 1
+  end 
 end
 
 def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  phrase_count = 0 
+  until phrase_count == number_of_times
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+    phrase_count += 1
+  end
 end
 
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  for i in (1..number_of_times)
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+  end 
 end
-
